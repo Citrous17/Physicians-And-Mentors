9658dbdb55999dcc94773e084daa60ac749afb42
Kyle Stallings <citrous@tamu.edu>
2025-02-11 15:41:18 -0600

    Renamed uid column to user_id in users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_11_204844) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_11_213924) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -71,7 +71,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_11_204844) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "uid"
+    t.string "user_id"
     t.string "provider"
     t.string "name"
     t.string "email"
