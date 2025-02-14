939e669e6c0b4abe3246fe4d35e692f1d8da4dda
nathan-on-git <156832505+nathan-on-git@users.noreply.github.com>
2025-02-14 14:16:22 -0600

    Added a common form for users and edited users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,34 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all 
   end
 
   def new
+    @user = User.new
   end
 
   def edit
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to users_path, notice: "User was successfully updated." }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_book
+      @user = User.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def user_params
+      params.require(:user).permit(:last_name, :first_name, :email, :password, :dob, :phone_number, :profile_image_url, :is_professional)
+    end
+
+
 end
---
+++ app/views/users/_form.html.rb
@@ -0,0 +1,59 @@
+<%= form_with(model: user) do |form| %>
+  <% if user.errors.any? %>
+    <div id="error_explanation">
+      <h2><%= pluralize(user.errors.count, "error") %> prohibited this user from being saved:</h2>
+
+      <ul>
+        <% user.errors.each do |error| %>
+          <li><%= error.full_message %></li>
+        <% end %>
+      </ul>
+    </div>
+  <% end %>
+
+  <div class="field">
+    <%= form.label :last_name %>
+    <%= form.text_field :first_name %>
+  </div>
+
+  <div class="field">
+    <%= form.label :last_name %>
+    <%= form.text_field :first_name %>
+  </div>
+
+  <div class="field">
+    <%= form.label :email %>
+    <%= form.number_field :email %>
+
+  </div>
+
+  <div class="field">
+    <%= form.label :password %>
+    <%= form.number_field :password %>
+  </div>
+
+  <div class="field">
+    <%= form.label :dob %>
+    <%= form.date_select :dob %>
+  </div>
+
+  <div class="field">
+    <%= form.label :phone_number %>
+    <%= form.number_field :phone_number %>
+  </div>
+
+  <div class="field">
+    <%= form.label :profile_image_url %>
+    <%= form.number_field :profile_image_url %>
+  </div>
+
+  <div class="field">
+    <%= form.label :is_professional %>
+    <%= form.number_field :is_professional %>
+  </div>
+
+
+  <div class="actions">
+    <%= form.submit %>
+  </div>
+<% end %>
