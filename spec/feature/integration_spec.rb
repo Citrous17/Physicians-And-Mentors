5e099b2d0fa99ab422ecb92c1904c040a3182cc3
nathan-on-git <156832505+nathan-on-git@users.noreply.github.com>
2025-02-15 12:17:20 -0600

    Modified test file for modified fields

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -6,15 +6,15 @@ RSpec.describe 'Creating a user', type: :feature do
     fill_in "user[first_name]", with: 'examplefirstname'
     fill_in "user[last_name]", with: 'examplelastname'
     fill_in "user[email]", with: 'email@provider.com'
-    fill_in "user[password]", with: 'mypassword'
-    select '2025', from: 'user[dob(1i)]' 
-    select 'January', from: 'user[dob(2i)]' 
-    select '1', from: 'user[dob(3i)]' 
+    fill_in "user[password_digest]", with: 'mypassword'
+    select '2025', from: 'user[DOB(1i)]' 
+    select 'January', from: 'user[DOB(2i)]' 
+    select '1', from: 'user[DOB(3i)]' 
     fill_in "user[phone_number]", with: '999999999'
     fill_in "user[profile_image_url]", with: 'profile.image.link'
-    fill_in "user[is_professional]", with: 'false'
+    fill_in "user[isProfessional]", with: 'false'
     click_on 'Create User'
-    visit physicians_path
+    visit users_path
 
     expect(page).to have_content('examplefirstname')
     expect(page).to have_content('examplelastname')
