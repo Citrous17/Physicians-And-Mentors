a268d86a10ea542851474d201c538da8749abeeb
Kyle Stallings <citrous@tamu.edu>
2025-02-15 12:32:13 -0600

    Change home landing page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "Signed out successfully!"
+    end
 end
